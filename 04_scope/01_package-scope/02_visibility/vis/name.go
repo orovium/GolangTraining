3e32a9e31dbbccdafbfb4e7fc7be6783a79b35a0
Javier Coronel Orovio <orovium@wiki-test.demiumstartups.com>
2018-05-28 16:00:36 +0200

    Scope + blank identifier finished

---
+++ 04_scope/01_package-scope/02_visibility/vis/name.go
@@ -0,0 +1,5 @@
+package vis
+
+// MyName is exported because it starts with a capital letter
+var MyName = "Javi"
+var yourName = "Future programmer"
---
+++ 04_scope/02_block-scope/02_closure/01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Println(x)
+	{
+		fmt.Println(x)
+		y := "Spam and more spam"
+		fmt.Println(y)
+	}
+
+	//fmt.Println(y) // outside scope of y
+}
