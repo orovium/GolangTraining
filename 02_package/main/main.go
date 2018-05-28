7a875042694419b0501f7c328aae1bad04724a0a
Javier Coronel Orovio <orovium@wiki-test.demiumstartups.com>
2018-05-28 14:03:23 +0200

    Using my own library

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+	"github.com/orovium/GolangTraining/02_package/stringutil"
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse(("!oG, olleH")))
+	fmt.Println(stringutil.Reverse(("!oG ,olleH")))
 }
