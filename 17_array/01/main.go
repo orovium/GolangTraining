17e355ee4ed4744e31886c18b5780ef13a0d1fc8
Javier Coronel Orovio <orovium@wiki-test.demiumstartups.com>
2018-06-01 13:45:43 +0200

    Arrays finished

---
+++ 17_array/01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var y []string  // this is an slice!!
+	var x [58]string // array has defined length
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(x[42])
+	for i := 65; i <= 122; i++ {
+		x[i-65] = string(i)
+	}
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(x[42])
+}
